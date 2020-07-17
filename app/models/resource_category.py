919944acd6d0d4245d284afb2515b7c93f450b88
littlefieldnick <nickolas.littlefield@maine.edu>
2020-07-16 21:04:25 -0400

    User and resource category classes (#2).
    User class represents an application user and has token functionality and account login confirmation

---
+++ app/models/resource_category.py
@@ -0,0 +1,10 @@
+from .. import db
+
+class ResourceCategory(db.Model):
+    """
+    Schema for categories associated with a resource
+    """
+
+    __tablename__ = 'resource_categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), index=True)
