f96b3f8058a2a82f092d6c6ce8c7f90312638aef
littlefieldnick <nickolas.littlefield@maine.edu>
2020-08-15 21:43:05 -0400

    Fixed error in config.py (#13)

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,10 @@ from .config import Config
 from .extensions import db, cors
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True, static_folder='static')
+    app = Flask(__name__, static_folder='static')
 
     # Configure basic app
     app.config.from_object(Config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
     # Configure Extensions
     configure_extensions(app)
@@ -26,10 +20,8 @@ def create_app():
     return app
 
 def configure_extensions(app):
-    # Register CORS
-    cors.init_app(app)
-
     # Register database
+    cors.init_app(app)
     from . import models
     db.init_app(app)
 
--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
+INSTANCE_PATH = 'instance/pm-dev.sqlite'
+
 class Config:
     DEBUG = True
-    SECRET_KEY = 'dev',
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://your_name:your_password@localhost:5432/smilecook'
+    SECRET_KEY = 'dev'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + INSTANCE_PATH
     SQLALCHEMY_TRACK_MODIFICATIONS = False
