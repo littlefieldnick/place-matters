18b8da3320bc16b99e6926453c9112dbe16d6b97
littlefieldnick <nickolas.littlefield@maine.edu>
2020-08-24 21:27:28 -0400

    Back End: Search API for searching resources (#12)

--- server/__init__.py
+++ server/__init__.py
@@ -44,12 +44,12 @@ def configure_extensions(app):
     db.init_app(app)
 
 def configure_api(app):
-    from server.api import resource_info, resource_category
+    from server.api import resource_info, resource_category, resource_search
     api = Api(app)
 
     api.add_resource(resource_info.ResourceInfoApi, "/resources/", "/resources/<int:id>")
     api.add_resource(resource_category.ResourceCategoryApi, "/categories/", "/categories/<int:id>")
-
+    api.add_resource(resource_search.ResourceInfoSearch, "/resources/search")
 def configure_cli(app):
     from server.cli.database import db_cli
     from server.cli.setup import setup_cli
--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,2 +1,3 @@
 from .resource_category import *
 from .resource_info import *
+from .resource_search import *
