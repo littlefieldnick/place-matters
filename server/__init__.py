9b5ca8790ff02fa621b53228fb3581f741c1a840
littlefieldnick <nickolas.littlefield@maine.edu>
2020-09-19 19:44:13 -0400

    Back End: Fixed issue with route for KML (#10)

--- server/__init__.py
+++ server/__init__.py
@@ -29,8 +29,8 @@ def create_app():
     # Configure API
     configure_api(app)
 
-    @app.route("./Maine_Boundaries_County_Polygon.kml")
-    @app.route("./maine_androscoggin.kml")
+    @app.route("/Maine_Boundaries_County_Polygon.kml")
+    @app.route("/maine_androscoggin.kml")
     def serve_kml():
         return send_from_directory(app.static_folder, filename="kml/" + request.path[1:])
 
