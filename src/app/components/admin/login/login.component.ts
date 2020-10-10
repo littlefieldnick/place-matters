8f1cea9c32c557ebe09cf144aa47c3fcac5e81cf
littlefieldnick <nickolas.littlefield@maine.edu>
2020-10-09 20:14:33 -0400

    Admin UI: Login/Registration and Starting Dashboard from Angular Material. (#17, #18)

---
+++ src/app/components/admin/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {LoginForm} from "../../../forms/login.form";
+import {AuthService} from "../../../services/auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: LoginForm
+  formSubmitted: boolean;
+  constructor(private authService: AuthService, private router: Router) {
+    this.loginForm = new LoginForm()
+    this.formSubmitted = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+  loginUser(){
+    this.formSubmitted = true;
+    if(this.loginForm.valid){
+      this.authService.loginUser(
+        this.loginForm.get("email").value,
+        this.loginForm.get("password").value
+      ).then((data) => {
+          if(data)
+            this.router.navigateByUrl("/dash")
+      });
+    }
+  }
+
+}
---
+++ src/app/components/admin/registration/registration.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {RegistrationForm} from "../../../forms/registration.form";
+import {AuthService} from "../../../services/auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+  registerForm: RegistrationForm
+  formSubmitted: boolean
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.registerForm = new RegistrationForm();
+    this.formSubmitted = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+  async registerUser() {
+    this.formSubmitted = true;
+    if (this.registerForm.valid) {
+      if (this.registerForm.get("password").value != this.registerForm.get("confirmPassword").value) {
+        console.log("Passwords do not match!");
+        return;
+      }
+
+      this.authService.register(this.registerForm.get("firstName").value,
+        this.registerForm.get("lastName").value,
+        this.registerForm.get("email").value,
+        this.registerForm.get("password").value
+      ).then((data) => {
+        if(!data)
+          return;
+
+        this.authService.loginUser(
+          this.registerForm.get("email").value,
+          this.registerForm.get("password").value
+        ).then(data => {
+          if(data)
+            this.router.navigateByUrl("/admin/dash");
+        });
+      });
+    }
+
+  }
+}
