5ccc17673ae80042d6d04ce422d0e9399d1a206c
littlefieldnick <nickolas.littlefield@maine.edu>
2020-11-12 19:50:27 -0500

    Updated DB Schema to work with MySQL for Bluehost deployment. (#23)

--- tables.sql
+++ tables.sql
@@ -1,41 +1,56 @@
-create TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                firstName TEXT,
-                                lastName TEXT,
-                                email TEXT UNIQUE,
-                                password TEXT,
-                                confirmPassword TEXT,
-                                CONSTRAINT email_unique UNIQUE(email));
+CREATE SCHEMA placematters;
+use `placematters`;
 
-create TABLE IF NOT EXISTS resource_category(id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                             categoryName TEXT);
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(45) DEFAULT NULL,
+  `lastName` varchar(45) DEFAULT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `confirmPassword` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
-create TABLE IF NOT EXISTS resource(id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    name TEXT,
-                                    street TEXT,
-                                    city TEXT,
-                                    zipcode TEXT,
-                                    state TEXT,
-                                    countyName INTEGER,
-                                    categoryName INTEGER,
-                                    description TEXT,
-                                    website TEXT,
-                                    latitude FLOAT,
-                                    longitude FLOAT,
-                                    FOREIGN KEY(countyName) REFERENCES county(id),
-                                    FOREIGN KEY(categoryName) REFERENCES resource_category(id));
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `categoryName` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
-create TABLE IF NOT EXISTS county(id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                  countyName TEXT);
+CREATE TABLE `county` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `countyName` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `resource` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `street` varchar(250) DEFAULT NULL,
+  `city` varchar(250) DEFAULT NULL,
+  `zip` varchar(250) DEFAULT NULL,
+  `countyName` int NOT NULL,
+  `categoryName` int DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `website` varchar(250) DEFAULT NULL,
+  `latitude` decimal(5,0) DEFAULT NULL,
+  `longitude` decimal(5,0) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `countyName_idx` (`countyName`),
+  KEY `categoryName_idx` (`categoryName`),
+  CONSTRAINT `categoryName` FOREIGN KEY (`categoryName`) REFERENCES `category` (`id`),
+  CONSTRAINT `countyName` FOREIGN KEY (`countyName`) REFERENCES `county` (`id`)
+);
 
 insert into  user(firstName, lastName, email, password, confirmPassword)
     VALUES ("Default", "Admin", "admin@admin.org", "482c811da5d5b4bc6d497ffa98491e38", "482c811da5d5b4bc6d497ffa98491e38");
 
-insert into resource_category(categoryName) values ("Community Reintegration");
-insert into resource_category(categoryName) values ("Early Intervention");
-insert into resource_category(categoryName) values ("Intensive Intervention");
-insert into resource_category(categoryName) values ("Intervention");
-insert into resource_category(categoryName) values ("Out of Home Treatment");
-insert into resource_category(categoryName) values ("Prevention");
+insert into category(categoryName) values ("Community Reintegration");
+insert into category(categoryName) values ("Early Intervention");
+insert into category(categoryName) values ("Intensive Intervention");
+insert into category(categoryName) values ("Intervention");
+insert into category(categoryName) values ("Out of Home Treatment");
+insert into category(categoryName) values ("Prevention");
 
 insert into  county(countyName) values("Androscoggin County");
 insert into  county(countyName) values("Aroostook County");
@@ -52,4 +67,4 @@ insert into  county(countyName) values("Sagadahoc County");
 insert into  county(countyName) values("Somerset County");
 insert into  county(countyName) values("Waldo County");
 insert into  county(countyName) values("Washington County");
-insert into  county(countyName) values("York County");
+insert into  county(countyName) values("York County");county
